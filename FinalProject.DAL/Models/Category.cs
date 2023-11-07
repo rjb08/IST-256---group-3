d5d1d5479d8aa57141e3c81df6d6a7e0a3d8e64e
Josh Rogan <jmr7831@psu.edu>
2023-11-07 16:05:54 -0500

    Revert "Initial categories work"
    This reverts commit aa775b37f8c0f8792c14d6a4a2f7667a29229ebe.

--- FinalProject.DAL/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalProject.DAL.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
-    }
-}
